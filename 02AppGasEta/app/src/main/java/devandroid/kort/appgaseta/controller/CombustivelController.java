308b405014521c747da664435e86d19b513581e5
danielkort <daniel1kort@gmail.com>
2024-11-13 13:02:57 -0300

    Implementacao metodo alterar objeto do DB no AppGasEta

--- 02AppGasEta/app/src/main/java/devandroid/kort/appgaseta/controller/CombustivelController.java
+++ 02AppGasEta/app/src/main/java/devandroid/kort/appgaseta/controller/CombustivelController.java
@@ -47,6 +47,19 @@ public class CombustivelController extends GasEtaDB {
 
     }
 
+    public void alterar(Combustivel combustivel){
+
+        ContentValues dados = new ContentValues();
+
+        dados.put("id", combustivel.getId());;
+        dados.put("nomeDoCombustivel", combustivel.getNomeDoCombustivel());
+        dados.put("precoDoCombustivel", combustivel.getPrecoDoCombustivel());
+        dados.put("recomendacao", combustivel.getRecomendacao());
+
+        alterarObjeto("Combustivel",dados);
+
+    }
+
     public void limpar(){
         dadosPreferences.clear();
         dadosPreferences.apply();
--- 02AppGasEta/app/src/main/java/devandroid/kort/appgaseta/database/GasEtaDB.java
+++ 02AppGasEta/app/src/main/java/devandroid/kort/appgaseta/database/GasEtaDB.java
@@ -86,4 +86,14 @@ public class GasEtaDB extends SQLiteOpenHelper {
 
     }
 
+    public void alterarObjeto(String tabela,
+                              ContentValues dados){
+
+        int id = dados.getAsInteger("id");
+
+        db.update(tabela,dados,"id=?",
+                new String[]{Integer.toString(id)});
+
+    }
+
 }
